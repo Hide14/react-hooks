8b708c863082ca16bf3033ae130b9a1a036b9845
EC2 Default User <ec2-user@ip-172-31-91-122.ec2.internal>
2020-04-14 05:56:20 +0000

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,32 +1,50 @@
 import React, {useState, useContext} from 'react'
 import  {
   CREATE_EVENT,
-  DELETE_ALL_EVENT
+  DELETE_ALL_EVENT,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS
 } from '../actions'
 
 import AppContext from '../contexts/AppContext'
+import {timeCurrentIso8601} from '../utils'
 
 const EventForm = () => {
     const {state, dispatch} = useContext(AppContext)
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
-      const addEvent = e => {
-        e.preventDefault()
-        console.log({title, body})
-        dispatch({
-          type:CREATE_EVENT,
-          title,
-          body
-        })
-        setTitle('')
-        setBody('')
-      }
+    const addEvent = e => {
+      e.preventDefault()
+      console.log({title, body})
+      dispatch({
+        type:CREATE_EVENT,
+        title,
+        body
+      })
+      
+      dispatch({
+        type:ADD_OPERATION_LOG,
+        description:'イベントを作成しました。',
+        operatedAt:timeCurrentIso8601()
+      })
+      
+      setTitle('')
+      setBody('')
+    }
       
       console.log({state})
       const deleteAllEvents = e => {
         e.preventDefault()
         const result = window.confirm('全てのイベントを本当に削除しても良いですか')
-        if (result) dispatch({type:DELETE_ALL_EVENT})
+        if (result) {
+          dispatch({type:DELETE_ALL_EVENT})
+          
+          dispatch({
+            type:ADD_OPERATION_LOG,
+            description:'全てのイベントを削除しました。',
+            operatedAt:timeCurrentIso8601()
+          })
+        }
       }
       
       const unCreatable = title === '' || body === ''
